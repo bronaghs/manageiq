711baa5f146b306f4d785d348cd3dc74325175e0
Daniel Berger <dberger@redhat.com>
2015-06-18 09:30:54 -0600

    Start revamping parse_vm method.

--- vmdb/app/models/ems_refresh/parsers/azure.rb
+++ vmdb/app/models/ems_refresh/parsers/azure.rb
@@ -11,18 +11,16 @@ module EmsRefresh::Parsers
       @connection = ems.connect
       @options    = options || {}
       @data       = {}
+      @data_index = {}
     end
 
     def ems_inv_to_hashes
       log_header = "MIQ(#{self.class.name}.#{__method__}) Collecting data for EMS name: [#{@ems.name}] id: [#{@ems.id}]"
-
       $log.info("#{log_header}...")
 
       get_vms
 
       $log.info("#{log_header}...Complete")
-
-      @data
     end
 
     private
@@ -32,133 +30,49 @@ module EmsRefresh::Parsers
       $log.info("#{log_header} #{__method__}")
 
       vms = @connection.get_vms
-      process_collection(vms, :vms) { |instance| parse_vm(vms) }
+      process_collection(vms, :vms) { |instance| parse_vm(instance) }
     end
 
     def process_collection(collection, key)
       @data[key] ||= []
 
       collection.each do |item|
-        uid, new_result = yield(item)
-        next if uid.nil?
-
-        @data[key] << new_result
-        @data_index.store_path(key, uid, new_result)
+        @data[key] << item
       end
     end
 
+    # Parse hardware information from the hardwareProfile setting.
+    #
+    def process_vm_hardware(vm)
+      hardware = vm['properties']['hardwareProfile']
 
-    def parse_image(image, is_public)
-      uid      = image.id
-      location = image.location
-      guest_os = (image.platform == "windows") ? "windows" : "linux"
-
-      name     = get_name_from_tags(image)
-      name   ||= image.name
-      name   ||= $1 if location =~ /^(.+?)(\.(image|img))?\.manifest\.xml$/
-      name   ||= uid
-
-      new_result = {
-        :type            => "TemplateAmazon",
-        :uid_ems         => uid,
-        :ems_ref         => uid,
-        :name            => name,
-        :location        => location,
-        :vendor          => "amazon",
-        :raw_power_state => "never",
-        :template        => true,
-        # the is_public flag here avoids having to make an additional API call
-        # per image, since we already know whether it's a public image
-        :publicly_available => is_public,
-
-        :hardware    => {
-          :guest_os            => guest_os,
-          :bitness             => ARCHITECTURE_TO_BITNESS[image.architecture],
-          :virtualization_type => image.virtualization_type,
-          :root_device_type    => image.root_device_type,
-        },
+      {
+        :platform_guest_agent => hardware['platformGuestAgent'],
+        :size                 => hardware['size'],
+        :deployment_name      => hardware['deploymentName'],
+        :deployment_id        => hardware['deploymentId'],
+        :deployment_label     => hardware['deploymentLabel']
+        :deployment_locked    => hardware['deploymentLocked']
       }
-
-      return uid, new_result
     end
 
-    def parse_vm(instance)
+    # Parse a VM instance, where +instance+ is a hash of values returned by
+    # the VirtualMachineManager#list method.
+    #
+    def parse_vm(vm)
       log_header = "MIQ(#{self.class.name}.#{__method__})"
       $log.info("#{log_header} #{__method__}")
-
-      status = instance.status
-      return if @options["ignore_terminated_instances"] && status == :terminated
-
-      uid    = instance.id
-      name   = get_name_from_tags(instance)
-      name ||= uid
-
-      flavor_uid = instance.instance_type
-      @known_flavors << flavor_uid
-      flavor = @data_index.fetch_path(:flavors, flavor_uid) ||
-        @data_index.fetch_path(:flavors, "unknown")
-
-      private_network = {
-        :ipaddress => instance.private_ip_address,
-        :hostname  => instance.private_dns_name
-      }.delete_nils
-
-      public_network = {
-        :ipaddress => instance.public_ip_address,
-        :hostname  => instance.public_dns_name
-      }.delete_nils
-
-      parent_image = @data_index.fetch_path(:vms, instance.image_id)
-      if parent_image
-        virtualization_type = parent_image.fetch_path(:hardware, :virtualization_type)
-        root_device_type    = parent_image.fetch_path(:hardware, :root_device_type)
-      end
+      $log.info(instance)
 
       new_result = {
-        :type            => "VmAmazon",
-        :uid_ems         => uid,
-        :ems_ref         => uid,
-        :name            => name,
-        :vendor          => "amazon",
-        :raw_power_state => status.to_s,
-
-        :hardware    => {
-          :bitness             => ARCHITECTURE_TO_BITNESS[instance.architecture],
-          :virtualization_type => virtualization_type,
-          :root_device_type    => root_device_type,
-          :numvcpus            => flavor[:cpus],
-          :cores_per_socket    => 1,
-          :logical_cpus        => flavor[:cpus],
-          :memory_cpu          => flavor[:memory] / (1024 * 1024), # memory_cpu is in megabytes
-          :disk_capacity       => flavor[:disk_size],
-          :disks               => [], # Filled in later conditionally on flavor
-          :networks            => [], # Filled in later conditionally on what's available
-        },
-
-        :availability_zone   => @data_index.fetch_path(:availability_zones, instance.availability_zone),
-        :flavor              => flavor,
-        :cloud_network       => @data_index.fetch_path(:cloud_networks, instance.vpc_id),
-        :cloud_subnet        => @data_index.fetch_path(:cloud_subnets, instance.subnet_id),
-        :key_pairs           => [@data_index.fetch_path(:key_pairs, instance.key_name)].compact,
-        :security_groups     => instance.security_groups.to_a.collect { |sg| @data_index.fetch_path(:security_groups, sg.id) }.compact,
-        :orchestration_stack => @data_index.fetch_path(:orchestration_stacks, instance.tags["aws:cloudformation:stack-id"]),
+        :type            => vm['type']
+        :uid_ems         => vm['id']
+        :ems_ref         => vm['id']
+        :name            => vm['name']
+        :vendor          => "Microsoft",
+        :raw_power_state => vm['instanceView']['powerState'],
+        :hardware        => parse_hardware(vm)
       }
-      new_result[:location] = public_network[:hostname] if public_network[:hostname]
-      new_result[:hardware][:networks] << private_network.merge(:description => "private") unless private_network.blank?
-      new_result[:hardware][:networks] << public_network.merge(:description => "public")   unless public_network.blank?
-
-      if parent_image
-        new_result[:parent_vm] = parent_image
-        new_result.store_path(:hardware, :guest_os, parent_image.fetch_path(:hardware, :guest_os))
-      end
-
-      if flavor[:disk_count] > 0
-        disks = new_result[:hardware][:disks]
-        single_disk_size = flavor[:disk_size] / flavor[:disk_count]
-        flavor[:disk_count].times do |i|
-          add_instance_disk(disks, single_disk_size, i, "Disk #{i}")
-        end
-      end
 
       return uid, new_result
     end
