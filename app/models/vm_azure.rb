19ac34dfcfc0b4eb08d327e28a089039d78cb050
Daniel Berger <dberger@redhat.com>
2015-08-27 12:51:42 -0600

    Modify provider object to just return the armrest connection object, and updated the calculate_power_state method to handle states relevant to azure.
    Return unknown instead of the raw_power_state in else clause, and use regex instead of literal string for checks.

--- app/models/vm_azure.rb
+++ app/models/vm_azure.rb
@@ -1,7 +1,6 @@
 class VmAzure < VmCloud
   def provider_object(connection = nil)
     connection ||= ext_management_system.connect
-    connection.instances[ems_ref]
   end
 
   #
@@ -25,10 +24,13 @@ class VmAzure < VmCloud
   end
 
   def self.calculate_power_state(raw_power_state)
-    case raw_power_state
-    when "VM running" then "on"
-    when "VM deallocated", "VM deallocating" then "off"
-    else "unknown"
+    case raw_power_state.downcase
+    when /running/, /starting/
+      "On"
+    when /stopped/, /stopping/
+      "Off"
+    else
+      "Unknown"
     end
   end
 end
